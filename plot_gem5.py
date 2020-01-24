e06ece4a2f8d11e6fb9b406efcd53b4404520d82
YangZhou1997 <yangzhou@g.harvard.edu>
2020-01-24 16:28:19 -0500

    add plots for gem5

--- plot_gem5.py
+++ plot_gem5.py
@@ -11,6 +11,7 @@ import brewer2mpl
 import glob
 import re
 from termcolor import colored
+from util_serilize import *
 
 
  # brewer2mpl.get_map args: set name  set type  number of colors
@@ -41,9 +42,10 @@ dx = 0/72.; dy = -15/72.
 offset = matplotlib.transforms.ScaledTranslation(dx, dy, plt.gcf().dpi_scale_trans)
 
 
-nfinvoke = ['acl-fw', 'dpi-queue', 'nat-tcp-v4', 'maglev', 'lpm', 'monitoring']
+nfinvoke = ['acl-fw', 'dpi', 'nat-tcp-v4', 'maglev', 'lpm', 'monitoring']
+nfinvoke_legend = ["FW", "DPI", "NAT", "LB", "LPM", "Mon."]
 cpus = ['TimingSimpleCPU', 'DerivO3CPU']
-l2_size = ['4MB', '2MB', '1MB', '512kB', '256kB']
+l2_size = ['4kB', '8kB', '16kB', '32kB', '64kB', '128kB', '256kB', '512kB', '1MB', '2MB', '4MB']
 datadir = 'gem5data2'
 
 singleprog = nfinvoke
@@ -139,12 +141,11 @@ def load_data_cache():
             print('throughput', cpu, nf, corun_nfs, cachesize, th_value)
 
 
-
 def extract_m5out(f_name):
     found = re.search('m5out\/(.+?)\/', f_name).group(1)
     return found
 
-def extract_miss_rate(contents, cpu_ids):
+def extract_miss_rate(contents, nf, cpu_ids):
     if contents == '':
         print(colored('Error: m5out content null', 'red'))
         return 1
@@ -171,14 +172,31 @@ def extract_miss_rate(contents, cpu_ids):
                 overall_misses += int(line.split()[1])
             if f'system.l2.overall_misses::.switch_cpus{cpu_id}.inst' in line:
                 overall_misses += int(line.split()[1])
+            
+            if nf == 'dpi':
+                pure_cpu_id = cpu_id.strip('0')
+                if f'system.cpu{pure_cpu_id}.dcache.overall_accesses::.switch_cpus{cpu_id}.data' in line:
+                    overall_accesses += int(line.split()[1])
+                if f'system.cpu{pure_cpu_id}.icache.overall_accesses::.switch_cpus{cpu_id}.inst' in line:
+                    overall_accesses += int(line.split()[1])
+
+                if f'system.cpu{pure_cpu_id}.dcache.overall_hits::.switch_cpus{cpu_id}.data' in line:
+                    overall_hits += int(line.split()[1])
+                if f'system.cpu{pure_cpu_id}.icache.overall_hits::.switch_cpus{cpu_id}.inst' in line:
+                    overall_hits += int(line.split()[1])
+
+                if f'system.cpu{pure_cpu_id}.dcache.overall_misses::.switch_cpus{cpu_id}.data' in line:
+                    overall_misses += int(line.split()[1])
+                if f'system.cpu{pure_cpu_id}.icache.overall_misses::.switch_cpus{cpu_id}.inst' in line:
+                    overall_misses += int(line.split()[1])
 
     if overall_accesses == 0:
         print(colored('Error: m5out overall_accesses is zero', 'red'))
         return 1
-    if overall_hits != 0:
-        return 1- overall_hits * 1.0 / overall_accesses
     if overall_misses != 0:
         return overall_misses * 1.0 / overall_accesses
+    if overall_hits != 0:
+        return 1- overall_hits * 1.0 / overall_accesses
     
     print(colored('Error: m5out no overall_hits and overall_misses', 'red'))
     return 1
@@ -246,12 +264,97 @@ def load_data_bus():
             # print(nf)
             # print(cpu_ids)
 
-            miss_rate = extract_miss_rate(contents, cpu_ids)
+            miss_rate = extract_miss_rate(contents, nf, cpu_ids)
 
             rawdata['l2missrate'][cpu][nf][corun_nfs][cachesize] = miss_rate
             print('l2missrate', cpu, nf, corun_nfs, cachesize, miss_rate)
-            
+         
         # print('')
+
+def get_datavec_vary_cachesize(_type, _cpu, _nf):
+    data_vec = list()
+    for _cachesize in l2_size:
+        data_vec.append(rawdata[_type][_cpu][_nf]["standalone"][_cachesize])
+    return data_vec
+
+# type: throughput or l2missrate
+def plot_vary_cachesize(_type, _cpu):
+    N = len(l2_size)
+    ind = np.arange(N) * 10 + 10    # the x locations for the groups    
+    width = 6.0/N       # the width of the bars: can also be len(x) sequence
+
+    cnt = 0
+    legends = list()
+    for _nf in singleprog:
+        data_vec = get_datavec_vary_cachesize(_type, _cpu, _nf)
+        p1, = plt.plot(ind, data_vec, linestyle = linestyles[cnt], marker = markers[cnt], markersize = markersizes[cnt],
+            color=colors[cnt], linewidth=3)
+        legends.append(p1)
+        cnt += 1
+
+    plt.legend(legends, nfinvoke_legend, loc='best', ncol=2)
+    if _type == 'throughput':
+        plt.ylabel('Throughput (Mpps)')
+    elif _type == 'l2missrate':
+        plt.ylabel('L2 cache missing rate')
+        
+    plt.xticks(ind, l2_size, rotation=45, ha="right", rotation_mode="anchor")
+
+    # apply offset transform to all x ticklabels.
+    for label in plt.axes().xaxis.get_majorticklabels():
+        label.set_transform(label.get_transform() + offset)
+
+    plt.tight_layout()
+    plt.savefig(f'./figures/gem5/cachesize_{_type}_{_cpu}.pdf')
+    plt.clf()
+
+def get_datavec_vary_corun(_type, _cpu, _nf):
+    data_vec = [0.0 for nf in singleprog]
+    nf_combs = multiprog.copy()
+    nf_combs.extend(singleprog)
+
+    for nf_comb in nf_combs:
+        if _nf in nf_comb:
+            dot_num = nf_comb.count('.')
+            if dot_num == 0:
+                data_vec[dot_num] += rawdata[_type][_cpu][_nf]["standalone"]['4MB']
+            else:
+                temp = nf_comb.split('.')
+                temp.remove(_nf)
+                nf_comb_exclude = prog_set_to_cmd(temp)
+                data_vec[dot_num] += rawdata[_type][_cpu][_nf][nf_comb_exclude]['4MB']
+    return data_vec
+
+# type: throughput or l2missrate
+def plot_vary_corun(_type, _cpu):
+    N = len(singleprog)
+    ind = np.arange(N) * 10 + 10    # the x locations for the groups    
+    width = 6.0/N       # the width of the bars: can also be len(x) sequence
+
+    cnt = 0
+    legends = list()
+    for _nf in singleprog:
+        data_vec = get_datavec_vary_corun(_type, _cpu, _nf)
+        p1, = plt.plot(ind, data_vec, linestyle = linestyles[cnt], marker = markers[cnt], markersize = markersizes[cnt],
+            color=colors[cnt], linewidth=3)
+        legends.append(p1)
+        cnt += 1
+
+    plt.legend(legends, nfinvoke_legend, loc='best', ncol=2)
+    if _type == 'throughput':
+        plt.ylabel('Throughput (Mpps)')
+    elif _type == 'l2missrate':
+        plt.ylabel('L2 cache missing rate')
+        
+    plt.xticks(ind, ['Standalone', 'Co-locate\nwith 1 NF', 'Co-locate\nwith 2 NFs', 'Co-locate\nwith 3 NFs', 'Co-locate\nwith 4 NFs', 'Co-locate\nwith 5 NFs'], rotation=45, ha="right", rotation_mode="anchor", fontsize=24)
+
+    # apply offset transform to all x ticklabels.
+    for label in plt.axes().xaxis.get_majorticklabels():
+        label.set_transform(label.get_transform() + offset)
+
+    plt.tight_layout()
+    plt.savefig(f'./figures/gem5/corun_{_type}_{_cpu}.pdf')
+    plt.clf()
 
 
 if __name__ == '__main__':
@@ -261,4 +364,11 @@ if __name__ == '__main__':
        fname = '/usr/share/fonts/truetype/adf/GilliusADF-Regular.otf')
 
     # load_data_cache()
-    load_data_bus()
+    # load_data_bus()
+    # write_to_file(rawdata, f'./{datadir}/drawdata/thrput_l2miss.res')
+
+    rawdata = read_from_file(f'./{datadir}/drawdata/thrput_l2miss.res')
+    for _type in ['throughput', 'l2missrate']:
+        for _cpu in cpus:
+            plot_vary_cachesize(_type, _cpu)
+            plot_vary_corun(_type, _cpu)
