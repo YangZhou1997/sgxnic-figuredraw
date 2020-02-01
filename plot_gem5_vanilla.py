2efecdbd277ea4c78322a7db0cdf75bd450d9ec5
YangZhou1997 <yangzhou@g.harvard.edu>
2020-02-01 14:39:00 -0500

    add plots for gem5

--- plot_gem5_vanilla.py
+++ plot_gem5_vanilla.py
@@ -20,29 +20,19 @@ offset = matplotlib.transforms.ScaledTranslation(dx, dy, plt.gcf().dpi_scale_tra
 
 nfinvoke = ['acl-fw', 'dpi', 'nat-tcp-v4', 'maglev', 'lpm', 'monitoring']
 nfinvoke_legend = ["FW", "DPI", "NAT", "LB", "LPM", "Mon."]
-cpus = ['detailed']
-modes = ['none', 'tp']
-l2_size = ['256kB', '512kB', '1MB', '2MB', '4MB']
-datadir = 'gem5data/tp_10mins'
-
-def bit_num(x):
-    cnt = 0
-    for i in range(32):
-        if ((x >> i) & 1) == 1:
-            cnt += 1
-    return cnt        
+cpus = ['TimingSimpleCPU', 'DerivO3CPU']
+l2_size = ['4kB', '8kB', '16kB', '32kB', '64kB', '128kB', '256kB', '512kB', '1MB', '2MB', '4MB']
+datadir = 'gem5data_100kflows'
 
 singleprog = nfinvoke
 multiprog = []
 for i in range(1, 1 << 6):
     prog_set = []
-    bitn = bit_num(i)
-    if bitn not in [2, 4]:
-        continue
     for j in range(6):
         if (i >> j) & 1 == 1:
             prog_set.append(nfinvoke[j])
     multiprog.append(prog_set)
+multiprog = list(filter(lambda x: len(x) > 1, multiprog))
 # print(multiprog, len(multiprog))
 
 def prog_set_to_cmd(prog_set):
@@ -55,15 +45,15 @@ def prog_set_to_cmd(prog_set):
     return ret
 multiprog = list(map(lambda x: prog_set_to_cmd(x), multiprog))
 
-# "throughput"/"l2missrate" -> "detailed" -> "monitoring" -> "standalone"/"nat-tcp-v4.lpm" -> "l2 cache size" -> "tp"/"none" -> value
-rawdata = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(float))))))
+# "throughput" -> "TimingSimpleCPU" -> "monitoring" -> "standalone"/"nat-tcp-v4" -> "l2 cache"
+rawdata = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(float)))))
 
 def extract_stdout(f_name):
     found = re.search('stdout_(.+?)\.out', f_name).group(1)
     return found
 
 ticks_base = 1000000000000 # one second
-def extract_lasting_times_contents(contents):
+def extract_lasting_times(contents):
     if contents == '':
         print(colored('Error: stdout content null', 'red'))
         return 1
@@ -71,27 +61,18 @@ def extract_lasting_times_contents(contents):
     r = re.search('Switched\ CPUS\ \@\ tick\ (.+?)\n', contents)
     if r: 
         start_ticks = int(r.group(1))
-        start_pos = r.start()
     else:
         start_ticks = 0
-        start_pos = 0
         print(colored('start_ticks not found', 'red'))
 
-    r = re.search('reached\ the\ max\ instruction\ count\ \@\ (.+?)\n', contents)
+    r = re.search('Exiting\ \@\ tick\ (.+?)\ ', contents)
     if r:
         end_ticks = int(r.group(1))
-        end_pos = r.start()
     else:
-        r = re.search('Exiting\ \@\ tick\ (.+?)\ ', contents)
-        if r: 
-            end_ticks = int(r.group(1))
-            end_pos = r.start()
-        else:
-            end_ticks = 5 * ticks_base
-            end_pos = len(contents)
-            print(colored('end_ticks not found', 'red'))
+        end_ticks = 5 * ticks_base    
+        print(colored('end_ticks not found', 'red'))
 
-    return (end_ticks - start_ticks) * 1.0 / ticks_base, contents[start_pos: end_pos]
+    return (end_ticks - start_ticks) * 1.0 / ticks_base
 
 def extract_packet_num(contents, nf):
     lines = contents.split('\n')
@@ -121,9 +102,8 @@ def load_data_throughput():
 
         splits = extract_stdout(f_name).split('_')
         cpu = splits[0]
-        nfs_str = splits[1]
+        nfs_str = splits[1].replace('dpi-queue', 'dpi')
         cachesize = splits[2]
-        mode = splits[3]
 
         contents = open(f_name).read()
         index = contents.find('Switched CPUS @ tick ')
@@ -131,9 +111,9 @@ def load_data_throughput():
 
         nfs = nfs_str.split('.')
         
-        lasting_time, sim_contents = extract_lasting_times_contents(contents)
+        lasting_time = extract_lasting_times(contents)
         for nf in nfs:
-            packet_num = extract_packet_num(sim_contents, nf)
+            packet_num = extract_packet_num(contents, nf)
             th_value = packet_num / lasting_time / (1000000)
             
             corun_nfs_list = nfs.copy()
@@ -142,9 +122,9 @@ def load_data_throughput():
             if corun_nfs == '':
                 corun_nfs = 'standalone'
             
-            rawdata['throughput'][cpu][nf][corun_nfs][cachesize][mode] = th_value
+            rawdata['throughput'][cpu][nf][corun_nfs][cachesize] = th_value
 
-            print('throughput', cpu, nf, corun_nfs, cachesize, mode, th_value)
+            print('throughput', cpu, nf, corun_nfs, cachesize, th_value)
         print('')
 
 
@@ -152,67 +132,96 @@ def extract_m5out(f_name):
     found = re.search('m5out\/(.+?)\/', f_name).group(1)
     return found
 
-def extract_miss_rate(contents, nf, cpu_id, num_nfs, mode):
+def extract_miss_rate(contents, nf, cpu_ids):
     if contents == '':
         print(colored('Error: m5out content null', 'red'))
         return 1
 
     lines = contents.split('\n')
-
-    if num_nfs == 1:
+    overall_accesses = 0
+    overall_hits = 0
+    overall_misses = 0
+    # print(cpu_ids)
+    for cpu_id in cpu_ids:
+        # system.l2.[overall_accesses|overall_hits|overall_misses]::.switch_cpus[cpu_id].data|inst
         for line in lines:
-            if 'system.l2.overall_miss_rate::total' in line:
-                miss_rate = float(line.split()[1])
-                return miss_rate
-        print(colored('Error: m5out no l2.overall_miss_rate', 'red'))            
+            if f'system.l2.overall_accesses::.switch_cpus{cpu_id}.data' in line:
+                overall_accesses += int(line.split()[1])
+            if f'system.l2.overall_accesses::.switch_cpus{cpu_id}.inst' in line:
+                overall_accesses += int(line.split()[1])
+
+            if f'system.l2.overall_hits::.switch_cpus{cpu_id}.data' in line:
+                overall_hits += int(line.split()[1])
+            if f'system.l2.overall_hits::.switch_cpus{cpu_id}.inst' in line:
+                overall_hits += int(line.split()[1])
+
+            if f'system.l2.overall_misses::.switch_cpus{cpu_id}.data' in line:
+                overall_misses += int(line.split()[1])
+            if f'system.l2.overall_misses::.switch_cpus{cpu_id}.inst' in line:
+                overall_misses += int(line.split()[1])
+            
+            # extract missrate for 16 dpi threads;
+            # if nf == 'dpi':
+            #     pure_cpu_id = cpu_id.strip('0')
+            #     if f'system.cpu{pure_cpu_id}.dcache.overall_accesses::.switch_cpus{cpu_id}.data' in line:
+            #         overall_accesses += int(line.split()[1])
+            #     if f'system.cpu{pure_cpu_id}.icache.overall_accesses::.switch_cpus{cpu_id}.inst' in line:
+            #         overall_accesses += int(line.split()[1])
+
+            #     if f'system.cpu{pure_cpu_id}.dcache.overall_hits::.switch_cpus{cpu_id}.data' in line:
+            #         overall_hits += int(line.split()[1])
+            #     if f'system.cpu{pure_cpu_id}.icache.overall_hits::.switch_cpus{cpu_id}.inst' in line:
+            #         overall_hits += int(line.split()[1])
+
+            #     if f'system.cpu{pure_cpu_id}.dcache.overall_misses::.switch_cpus{cpu_id}.data' in line:
+            #         overall_misses += int(line.split()[1])
+            #     if f'system.cpu{pure_cpu_id}.icache.overall_misses::.switch_cpus{cpu_id}.inst' in line:
+            #         overall_misses += int(line.split()[1])
+    
+    # print(overall_accesses, overall_hits, overall_misses)
+
+    if overall_accesses == 0:
+        print(colored('Error: m5out overall_accesses is zero', 'red'))
         return 1
-    else: # num_nfs = 2 or 4:
-        if mode == 'tp':
-            for line in lines:
-                if f'system.l2{cpu_id}.overall_miss_rate::total' in line:
-                    miss_rate = float(line.split()[1])
-                    return miss_rate
-            print(colored(f'Error: m5out no l2{cpu_id}.overall_miss_rate', 'red'))            
-        else: # none
-            overall_accesses = 0
-            overall_hits = 0
-            overall_misses = 0
-            for line in lines:
-                if f'system.l2.overall_accesses::switch_cpus{cpu_id}.data' in line:
-                    overall_accesses += int(line.split()[1])
-                if f'system.l2.overall_accesses::switch_cpus{cpu_id}.inst' in line:
-                    overall_accesses += int(line.split()[1])
-
-                if f'system.l2.overall_hits::switch_cpus{cpu_id}.data' in line:
-                    overall_hits += int(line.split()[1])
-                if f'system.l2.overall_hits::switch_cpus{cpu_id}.inst' in line:
-                    overall_hits += int(line.split()[1])
-
-                if f'system.l2.overall_misses::switch_cpus{cpu_id}.data' in line:
-                    overall_misses += int(line.split()[1])
-                if f'system.l2.overall_misses::switch_cpus{cpu_id}.inst' in line:
-                    overall_misses += int(line.split()[1])
-            
-            if overall_accesses == 0:
-                print(colored('Error: m5out overall_accesses is zero', 'red'))
-                return 1
-            if overall_misses != 0:
-                return overall_misses * 1.0 / overall_accesses
-            if overall_hits != 0:
-                return 1- overall_hits * 1.0 / overall_accesses
-            
-            print(colored('Error: m5out no overall_hits and overall_misses', 'red'))
-            return 1
+    if overall_misses != 0:
+        return overall_misses * 1.0 / overall_accesses
+    if overall_hits != 0:
+        return 1- overall_hits * 1.0 / overall_accesses
+    
+    print(colored('Error: m5out no overall_hits and overall_misses', 'red'))
+    return 1
 
 # system.l2.demand_hits::.switch_cpus0.data
 def get_cpuids_from_name(nfs_str):
     nf_cpu_ids = defaultdict(lambda : [])
     nfs = nfs_str.split('.')
-    idx = 0
-    for nf in nfs:
-        nf_cpu_ids[nf] = f'{idx}'
-        idx += 1
+    nf_num = len(nfs)
+    if 'dpi' in nfs:
+        nf_num += 16
+
+    if nf_num == 1:
+        nf_cpu_ids[nfs[0]] = ['']
+    elif nf_num < 10:
+        idx = 0
+        for nf in nfs:
+            if nf == 'dpi':
+                nf_cpu_ids[nf] = [f'{i}' for i in range(idx, idx + 16 + 1)]
+                idx += 16 + 1
+            else:
+                nf_cpu_ids[nf] = [f'{i}' for i in range(idx, idx + 1)]
+                idx += 1
+    else:
+        idx = 0
+        for nf in nfs:
+            if nf == 'dpi':
+                nf_cpu_ids[nf] = ['{:0>2d}'.format(i) for i in range(idx, idx + 16 + 1)]
+                idx += 16 + 1
+            else:
+                nf_cpu_ids[nf] = ['{:0>2d}'.format(i) for i in range(idx, idx + 1)]
+                idx += 1
+                            
     return nf_cpu_ids    
+
 
 def load_data_l2cachemiss():
     f_list = glob.glob(f'./{datadir}/m5out/*')
@@ -225,9 +234,8 @@ def load_data_l2cachemiss():
         
         splits = dir_name.split('_')
         cpu = splits[0]
-        nfs_str = splits[1]
+        nfs_str = splits[1].replace('dpi-queue', 'dpi')
         cachesize = splits[2]
-        mode = splits[3]
 
         f_name = f'{f_name}/{dir_name}_stats.txt'
         # print(f_name)
@@ -235,7 +243,6 @@ def load_data_l2cachemiss():
 
         nf_cpu_ids = get_cpuids_from_name(nfs_str)
         nfs = nfs_str.split('.')
-        num_nfs = len(nfs)
 
         for nf in nfs:
             corun_nfs_list = nfs.copy()
@@ -244,28 +251,21 @@ def load_data_l2cachemiss():
             if corun_nfs == '':
                 corun_nfs = 'standalone'
 
-            cpu_id = nf_cpu_ids[nf]
+            cpu_ids = nf_cpu_ids[nf]
             # print(nf)
             # print(cpu_ids)
 
-            miss_rate = extract_miss_rate(contents, nf, cpu_id, num_nfs, mode)
+            miss_rate = extract_miss_rate(contents, nf, cpu_ids)
 
-            rawdata['l2missrate'][cpu][nf][corun_nfs][cachesize][mode] = miss_rate
-            print('l2missrate', cpu, nf, corun_nfs, cachesize, mode, miss_rate)
+            rawdata['l2missrate'][cpu][nf][corun_nfs][cachesize] = miss_rate
+            print('l2missrate', cpu, nf, corun_nfs, cachesize, miss_rate)
          
         print('')
-
-
 
 def get_datavec_vary_cachesize(_type, _cpu, _nf):
     data_vec = list()
     for _cachesize in l2_size:
-        tp = rawdata[_type][_cpu][_nf]["standalone"][_cachesize]['tp']
-        none = rawdata[_type][_cpu][_nf]["standalone"][_cachesize]['none']
-        if _type == 'throughput':
-            data_vec.append((none - tp) / none)
-        else:
-            data_vec.append((tp - none) / none)
+        data_vec.append(rawdata[_type][_cpu][_nf]["standalone"][_cachesize])
     return data_vec
 
 # type: throughput or l2missrate
@@ -285,12 +285,12 @@ def plot_vary_cachesize(_type, _cpu):
 
     plt.legend(legends, nfinvoke_legend, loc='best', ncol=2, frameon=False)
     if _type == 'throughput':
-        plt.ylabel('Throughput degradation')
+        plt.ylabel('Throughput (Mpps)')
     elif _type == 'l2missrate':
-        plt.ylabel('L2 missing rate increasing')
-
+        plt.ylabel('L2 cache missing rate')
+        
     plt.xticks(ind, l2_size, rotation=45, ha="right", rotation_mode="anchor")
-    # plt.axes().set_ylim(ymin=0)
+    plt.axes().set_ylim(ymin=0)
 
     # apply offset transform to all x ticklabels.
     for label in plt.axes().xaxis.get_majorticklabels():
@@ -301,8 +301,8 @@ def plot_vary_cachesize(_type, _cpu):
     plt.clf()
 
 def get_datavec_vary_corun(_type, _cpu, _nf):
-    cnt_vec = [0, 0, 0, 0]    
-    data_vec = [0.0, 0.0, 0.0, 0.0]
+    cnt_vec = [0 for nf in singleprog]    
+    data_vec = [0.0 for nf in singleprog]
     nf_combs = multiprog.copy()
     nf_combs.extend(singleprog)
 
@@ -310,31 +310,20 @@ def get_datavec_vary_corun(_type, _cpu, _nf):
         if _nf in nf_comb:
             dot_num = nf_comb.count('.')
             if dot_num == 0:
-                tp = rawdata[_type][_cpu][_nf]["standalone"]['4MB']['tp']
-                none = rawdata[_type][_cpu][_nf]["standalone"]['4MB']['none']
-                if _type == 'throughput':
-                    data_vec[dot_num] += (none - tp) / none
-                else:
-                    data_vec[dot_num] += (tp - none) / none
+                data_vec[dot_num] += rawdata[_type][_cpu][_nf]["standalone"]['4MB']
             else:
                 temp = nf_comb.split('.')
                 temp.remove(_nf)
                 nf_comb_exclude = prog_set_to_cmd(temp)
-                tp = rawdata[_type][_cpu][_nf][nf_comb_exclude]['4MB']['tp']
-                none = rawdata[_type][_cpu][_nf][nf_comb_exclude]['4MB']['none']
-                if _type == 'throughput':
-                    data_vec[dot_num] += (none - tp) / none
-                else:
-                    data_vec[dot_num] += (tp - none) / none
+                data_vec[dot_num] += rawdata[_type][_cpu][_nf][nf_comb_exclude]['4MB']
             cnt_vec[dot_num] += 1
-    for i in [0, 1, 3]:
-        data_vec[i] /= cnt_vec[i] * 1.0
-    del data_vec[2]    
+    for i in range(len(singleprog)):
+        data_vec[i] /= cnt_vec[i] * 1.0                        
     return data_vec
 
 # type: throughput or l2missrate
 def plot_vary_corun(_type, _cpu):
-    N = 3
+    N = len(singleprog)
     ind = np.arange(N) * 10 + 10    # the x locations for the groups    
     width = 6.0/N       # the width of the bars: can also be len(x) sequence
 
@@ -349,12 +338,12 @@ def plot_vary_corun(_type, _cpu):
 
     plt.legend(legends, nfinvoke_legend, loc='best', ncol=2, frameon=False)
     if _type == 'throughput':
-        plt.ylabel('Throughput degradation')
+        plt.ylabel('Throughput (Mpps)')
     elif _type == 'l2missrate':
-        plt.ylabel('L2 missing rate increasing')
+        plt.ylabel('L2 cache missing rate')
         
-    plt.xticks(ind, ['Standalone', 'Co-locate \nwith 1 NF', 'Co-locate \nwith 3 NFs'], rotation=45, ha="right", rotation_mode="anchor", fontsize=24)
-    # plt.axes().set_ylim(ymin=0)
+    plt.xticks(ind, ['Standalone', 'Co-locate \nwith 1 NF', 'Co-locate \nwith 2 NFs', 'Co-locate \nwith 3 NFs', 'Co-locate \nwith 4 NFs', 'Co-locate \nwith 5 NFs'], rotation=45, ha="right", rotation_mode="anchor", fontsize=24)
+    plt.axes().set_ylim(ymin=0)
 
     # apply offset transform to all x ticklabels.
     for label in plt.axes().xaxis.get_majorticklabels():
