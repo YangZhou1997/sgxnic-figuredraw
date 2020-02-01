e7fbe5472d409f7c40bef55cbf24d0be26e9f4c1
YangZhou1997 <yangzhou@g.harvard.edu>
2020-02-01 11:35:11 -0500

    add 10M ins tp results

--- pull_gem5dat.sh
+++ pull_gem5dat.sh
@@ -6,13 +6,16 @@ node2=clnode165.clemson.cloudlab.us
 node3=clnode179.clemson.cloudlab.us
 node4=clnode122.clemson.cloudlab.us
 node5=clnode113.clemson.cloudlab.us
+node6=clnode104.clemson.cloudlab.us
+node7=clnode115.clemson.cloudlab.us
 
-m5out_dir=/users/yangzhou/gem5/sgx_nic/m5out/*
-stdout_dir=/users/yangzhou/gem5/sgx_nic/results/*
-stderr_dir=/users/yangzhou/gem5/sgx_nic/stderr/*
-scriptgen_dir=/users/yangzhou/gem5/sgx_nic/scriptgen/*
+m5out_dir=/users/yangzhou/GEM5_DRAMSim2/sgx_nic/m5out/*
+stdout_dir=/users/yangzhou/GEM5_DRAMSim2/sgx_nic/results/*
+stderr_dir=/users/yangzhou/GEM5_DRAMSim2/sgx_nic/stderr/*
+scriptgen_dir=/users/yangzhou/GEM5_DRAMSim2/sgx_nic/scriptgen/*
 
-datadir=gem5data
+datadir=gem5data/tp_10mins
+mkdir -p ./$datadir
 
 mkdir -p ./$datadir/m5out/
 mkdir -p ./$datadir/results/
@@ -20,10 +23,10 @@ mkdir -p ./$datadir/stderr/
 mkdir -p ./$datadir/scriptgen/
 mkdir -p ./$datadir/drawdata/
 
-for node in $node0 $node1 $node2 $node3 $node4 $node5
+for node in $node0 $node1 $node2 $node3 $node4 $node5 $node6 $node7
 do
     rsync -auv -e ssh yangzhou@$node:$m5out_dir ./$datadir/m5out/
     rsync -auv -e ssh yangzhou@$node:$stdout_dir ./$datadir/results/
-    # rsync -auv -e ssh yangzhou@$node:$stderr_dir ./$datadir/stderr/
+    rsync -auv -e ssh yangzhou@$node:$stderr_dir ./$datadir/stderr/
     rsync -auv -e ssh yangzhou@$node:$scriptgen_dir ./$datadir/scriptgen/
 done
